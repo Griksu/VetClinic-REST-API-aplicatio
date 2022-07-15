29efd47c1eeeb102b0ca2cad3698dd3a1b4161da
Grzegorz Wrobel <grzegorz.j.wrobel@gmail.com>
2022-07-15 23:41:30 +0200

    first commit

---
+++ src/main/java/com/crud/vetclinicback/VetclinicbackApplication.java
@@ -0,0 +1,13 @@
+package com.crud.vetclinicback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetclinicbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetclinicbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/vetclinicback/VetclinicbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.vetclinicback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetclinicbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
