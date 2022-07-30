ac3644bb8c20337c6a8d0db643ce906d0be2c9e5
Grzegorz Wrobel <grzegorz.j.wrobel@gmail.com>
2022-07-30 21:43:50 +0200

    OwnerController - final version

--- src/main/java/com/crud/vetclinicback/controller/OwnerController.java
+++ src/main/java/com/crud/vetclinicback/controller/OwnerController.java
@@ -1,41 +1,54 @@
 package com.crud.vetclinicback.controller;
 
+import com.crud.vetclinicback.exception.OwnerNotFoundException;
+import com.crud.vetclinicback.exception.PetNotFoundException;
+import com.crud.vetclinicback.mapper.OwnerMapper;
 import com.crud.vetclinicback.model.dto.OwnerDto;
+import com.crud.vetclinicback.model.entity.Owner;
+import com.crud.vetclinicback.service.OwnerDBService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/owners")
+@RequiredArgsConstructor
 public class OwnerController {
+    private final OwnerDBService ownerDBService;
+    private final OwnerMapper ownerMapper;
 
     @GetMapping
-    public List<OwnerDto> getOwners() {
-        return new ArrayList<>();
+    public ResponseEntity<List<OwnerDto>> getOwners() {
+        List<Owner> owners = ownerDBService.getOwners();
+        return ResponseEntity.ok(ownerMapper.mapToOwnerDtoList(owners));
     }
 
     @GetMapping(value = "{ownerId}")
-    public OwnerDto getOwner(@PathVariable Long ownerId) {
-        return new OwnerDto(1L, "John", "Smith",
-                "39 Main Street","New York", "001058779900",
-                new ArrayList<>());
+    public ResponseEntity<OwnerDto> getOwner(@PathVariable Long ownerId) throws
+            OwnerNotFoundException, PetNotFoundException {
+        return ResponseEntity.ok(ownerMapper.mapToOwnerDto(ownerDBService.getOwner(ownerId)));
     }
 
-    @DeleteMapping
-    public void deleteOwner(Long ownerId) {
-
+    @DeleteMapping(value = "{ownerId}")
+    public ResponseEntity<Void> deleteOwner(@PathVariable Long ownerId) {
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping
-    public OwnerDto updateOwner(OwnerDto ownerDto) {
-        return new OwnerDto(1L, "John", "Kowalski",
-                "39 Main Street", "New York", "001058779900",
-                new ArrayList<>());
+    public ResponseEntity<OwnerDto> updateOwner(@RequestBody OwnerDto ownerDto) throws
+            PetNotFoundException {
+        Owner owner = ownerMapper.mapToOwner(ownerDto);
+        Owner savedOwner = ownerDBService.saveOwner(owner);
+        return ResponseEntity.ok(ownerMapper.mapToOwnerDto(savedOwner));
     }
 
-    @PostMapping
-    public void createOwner(OwnerDto ownerDto) {
-
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createOwner(@RequestBody OwnerDto ownerDto) throws PetNotFoundException {
+        Owner owner = ownerMapper.mapToOwner(ownerDto);
+        ownerDBService.saveOwner(owner);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/crud/vetclinicback/mapper/OwnerMapper.java
+++ src/main/java/com/crud/vetclinicback/mapper/OwnerMapper.java
@@ -1,16 +1,20 @@
 package com.crud.vetclinicback.mapper;
 
+import com.crud.vetclinicback.exception.PetNotFoundException;
 import com.crud.vetclinicback.model.dto.OwnerDto;
 import com.crud.vetclinicback.model.entity.Owner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OwnerMapper {
     private final PetMapper petMapper;
 
-    public Owner mapToOwner(final OwnerDto ownerDto) {
+    public Owner mapToOwner(final OwnerDto ownerDto) throws PetNotFoundException {
         return new Owner(
                 ownerDto.getId(),
                 ownerDto.getFirstname(),
@@ -22,7 +26,7 @@ public class OwnerMapper {
         );
     }
 
-    public OwnerDto mapToOwnerDto(final Owner owner) {
+    public OwnerDto mapToOwnerDto(final Owner owner) throws PetNotFoundException {
         return new OwnerDto(
                 owner.getId(),
                 owner.getFirstname(),
@@ -32,5 +36,33 @@ public class OwnerMapper {
                 owner.getPhone(),
                 petMapper.mapToPetDtoList(owner.getPets())
         );
+    }
+
+    public List<Owner> mapToOwnerList(final List<OwnerDto> ownerDtoList) {
+        List<Owner> collect = new ArrayList<>();
+        for (OwnerDto ownerDto : ownerDtoList) {
+            Owner owner = null;
+            try {
+                owner = mapToOwner(ownerDto);
+            } catch (PetNotFoundException e) {
+                e.printStackTrace();
+            }
+            collect.add(owner);
+        }
+        return collect;
+    }
+
+    public List<OwnerDto> mapToOwnerDtoList(final List<Owner> ownerList) {
+        List<OwnerDto> collect = new ArrayList<>();
+        for (Owner owner : ownerList) {
+            OwnerDto ownerDto = null;
+            try {
+                ownerDto = mapToOwnerDto(owner);
+            } catch (PetNotFoundException e) {
+                e.printStackTrace();
+            }
+            collect.add(ownerDto);
+        }
+        return collect;
     }
 }
