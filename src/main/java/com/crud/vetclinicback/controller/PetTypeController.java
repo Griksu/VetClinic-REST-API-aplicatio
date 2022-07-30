982917b13fa22d484ca1de65577e6eff508f8d3d
Grzegorz Wrobel <grzegorz.j.wrobel@gmail.com>
2022-07-31 00:47:28 +0200

    PetTypeController - final version

---
+++ src/main/java/com/crud/vetclinicback/controller/PetTypeController.java
@@ -0,0 +1,55 @@
+package com.crud.vetclinicback.controller;
+
+import com.crud.vetclinicback.exception.PetNotFoundException;
+import com.crud.vetclinicback.exception.PetTypeNotFoundException;
+import com.crud.vetclinicback.mapper.PetTypeMapper;
+import com.crud.vetclinicback.model.dto.PetTypeDto;
+import com.crud.vetclinicback.model.entity.PetType;
+import com.crud.vetclinicback.service.PetTypeDBService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/petTypes")
+@RequiredArgsConstructor
+public class PetTypeController {
+    private final PetTypeDBService petTypeDBService;
+    private final PetTypeMapper petTypeMapper;
+
+    @GetMapping
+    public ResponseEntity<List<PetTypeDto>> getPetTypes() {
+        List<PetType> petTypes = petTypeDBService.getPetTypes();
+        return ResponseEntity.ok(petTypeMapper.mapToPetTypeDtoList(petTypes));
+    }
+
+    @GetMapping(value = "{petTypeId}")
+    public ResponseEntity<PetTypeDto> getPetType(@PathVariable Long petTypeId) throws
+            PetTypeNotFoundException, PetNotFoundException {
+        return ResponseEntity.ok(petTypeMapper.mapToPetTypeDto(petTypeDBService.getPetType(petTypeId)));
+    }
+
+    @DeleteMapping(value = "{petTypeId}")
+    public ResponseEntity<Void> deletePetType(@PathVariable Long petTypeId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<PetTypeDto> updatePetType(@RequestBody PetTypeDto petTypeDto) throws
+            PetTypeNotFoundException, PetNotFoundException {
+        PetType petType = petTypeMapper.mapToPetType(petTypeDto);
+        PetType savedPetType = petTypeDBService.savePetType(petType);
+        return ResponseEntity.ok(petTypeMapper.mapToPetTypeDto(savedPetType));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createPetType(@RequestBody PetTypeDto petTypeDto) throws
+            PetTypeNotFoundException {
+        PetType petType = petTypeMapper.mapToPetType(petTypeDto);
+        petTypeDBService.savePetType(petType);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/crud/vetclinicback/mapper/PetTypeMapper.java
+++ src/main/java/com/crud/vetclinicback/mapper/PetTypeMapper.java
@@ -1,16 +1,21 @@
 package com.crud.vetclinicback.mapper;
 
+import com.crud.vetclinicback.exception.PetNotFoundException;
+import com.crud.vetclinicback.exception.PetTypeNotFoundException;
 import com.crud.vetclinicback.model.dto.PetTypeDto;
 import com.crud.vetclinicback.model.entity.PetType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PetTypeMapper {
     private final PetMapper petMapper;
 
-    public PetType mapToPetType(final PetTypeDto petTypeDto) {
+    public PetType mapToPetType(final PetTypeDto petTypeDto) throws PetTypeNotFoundException {
         return new PetType(
                 petTypeDto.getId(),
                 petTypeDto.getTypeName(),
@@ -18,11 +23,25 @@ public class PetTypeMapper {
         );
     }
 
-    public PetTypeDto mapToPetTypeDto(final PetType petType) {
+    public PetTypeDto mapToPetTypeDto(final PetType petType) throws PetNotFoundException {
         return new PetTypeDto(
                 petType.getId(),
                 petType.getTypeName(),
                 petMapper.mapToPetDtoList(petType.getPets())
         );
+    }
+
+    public List<PetTypeDto> mapToPetTypeDtoList(final List<PetType> petTypeList) {
+        List<PetTypeDto> collect = new ArrayList<>();
+        for (PetType petType : petTypeList) {
+            PetTypeDto petTypeDto = null;
+            try {
+                petTypeDto = mapToPetTypeDto(petType);
+            } catch (PetNotFoundException e) {
+                e.printStackTrace();
+            }
+            collect.add(petTypeDto);
+        }
+        return collect;
     }
 }
